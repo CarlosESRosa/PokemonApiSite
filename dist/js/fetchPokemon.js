a88daf22ba42393c6aad659d0895c7706d772817
Carlos Eduardo <carlosrosa1001@gmail.com>
2021-12-04 20:14:21 -0300

    consumindo a api pokemon

---
+++ dist/js/fetchPokemon.js
@@ -0,0 +1,6 @@
+async function fetchPokemon(pokemonName) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+  const response = await fetch(url);
+  const data = response.json();
+  return data;
+};
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,17 @@
-console.log('hero');
+const firstInput = document.querySelector('#firstPokemon');
+const secondInput = document.querySelector('#secondPokemon');
+const button = document.querySelector('#runButton');
+const colLeft = document.querySelector('#colLeft');
+const colRight = document.querySelector('#colRight');
+
+button.addEventListener('click', () => {
+  console.log(firstInput.value);
+  console.log(secondInput.value);
+});
+
+window.onload = async () => {
+  const pokemonLeft = await fetchPokemon('vaporeon');
+  const pokemonRight = await fetchPokemon('flareon');
+  colLeft.innerHTML = pokemonLeft.name;
+  colRight.innerHTML = pokemonRight.name;
+}
