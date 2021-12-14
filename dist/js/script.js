d997545bf8a148e5d20b1a42fbb06899a986dd59
Carlos Eduardo <carlosrosa1001@gmail.com>
2021-12-14 00:21:54 -0300

    finalizado tabela de ambos pokemons

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,30 +1,53 @@
 const firstInput = document.querySelector('#firstPokemon');
 const secondInput = document.querySelector('#secondPokemon');
 const button = document.querySelector('#runButton');
+const tableStats = document.querySelector('#tableStats');
+const [tableLeft, tableRight] = Array.from(tableStats.children);
 
 button.addEventListener('click', async () => {
-    const dataPokemonLeft = await fetchPokemon(firstInput.value.toLowerCase());
-    const dataPokemonRight = await fetchPokemon(secondInput.value.toLowerCase());
-    createPokemonLeft(dataPokemonLeft.name, dataPokemonLeft.sprites.front_default);
-    createPokemonRight(dataPokemonRight.name, dataPokemonRight.sprites.front_default)
+  buildScreen();
+  const tableData = document.querySelector('#tableData');
+  tableData.style.display = 'block';
 });
 
+// Monta o pokemon 
+async function buildScreen() {
+  const dataPokemonLeft = await fetchPokemon(firstInput.value.toLowerCase());
+  const dataPokemonRight = await fetchPokemon(secondInput.value.toLowerCase());
+  createPokemonLeft(dataPokemonLeft.name, dataPokemonLeft.sprites.front_default);
+  createPokemonRight(dataPokemonRight.name, dataPokemonRight.sprites.front_default)
+  settingTableLeft(getPokemonStats(dataPokemonLeft));
+  settingTableRight(getPokemonStats(dataPokemonRight));
+}
+
+// Pega os status do pokemon
 function getPokemonStats(pokemon) {
   const { stats } = pokemon;
   const [hp, attack, defense, specialAttack, specialDefense, speed] = stats;
-  
+
   return stats.map((item) => {
-    const { stat, base_stat} = item;
-    console.log(stat.name, base_stat)
+    const { stat, base_stat } = item;
+    return {
+      stat: stat.name,
+      base_stat,
+    }
+  });
+}
+
+// Monta tabela da esquerda
+function settingTableLeft(pokemonLeft) {
+  Array.from(tableLeft.children).forEach((element, index) => {
+    element.innerText = `${pokemonLeft[index].stat}: ${pokemonLeft[index].base_stat} `;
+  });
+}
+// Monta tabela da direita
+function settingTableRight(pokemonRight) {
+  Array.from(tableRight.children).forEach((element, index) => {
+    element.innerText = `${pokemonRight[index].stat}: ${pokemonRight[index].base_stat} `;
   });
 }
 
 window.onload = async () => {
-  const pokemonLeft = await fetchPokemon('empoleon');
-  const pokemonRight = await fetchPokemon('milotic');
-  createPokemonLeft(pokemonLeft.name, pokemonLeft.sprites.front_default);
-  createPokemonRight(pokemonRight.name, pokemonRight.sprites.front_default)
-  
-  // getPokemonStats(pokemonLeft) 
-  // getPokemonStats(pokemonRight) 
+  const tableData = document.querySelector('#tableData');
+  tableData.style.display = 'none';
 }
